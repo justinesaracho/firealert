7ad290492ef9a5e6c9598459ea38964e5f7cae18
Justine Saracho <justine.saracho@gmail.comgit config --global user.email justine.saracho@gmail.comgit config --global user.email justine.saracho@gmail.com>
2020-11-26 23:48:25 +0800

    just uploaded everything

---
+++ app/src/main/java/com/example/raha_firealert/User/ui/safetytips/TipRecyclerView.java
@@ -0,0 +1,16 @@
+package com.example.raha_firealert.User.ui.safetytips;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.raha_firealert.R;
+
+public class TipRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_recycler_view);
+    }
+}
